922e88cecebaafe9e430b91de786c6278d2628d6
PoojaMukundan <40536525+PoojaMukundan@users.noreply.github.com>
2018-08-26 15:40:27 +0530

    DEMO ON ARRAYLIST

---
+++ TestCollection.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class TestCollection
+{
+ public static void main(String args[])
+{
+	ArrayList al=new ArrayList();
+	al.add(10);
+	al.add(20);
+	al.add("welcome");
+	al.add(3.4f);
+	System.out.println(al);
+}
+}
